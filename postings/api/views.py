cbae42331ca26310b0785666c3ef298ea8b9321f
Daniel Leszczyński <danielleszczynski@MacBook-Pro-Daniel.local>
2018-01-13 14:23:04 +0100

    Add postings app with first blog post REST endpoint

---
+++ postings/api/views.py
@@ -0,0 +1,15 @@
+from rest_framework import generics
+from .serializers import BlogPostSerializer
+from postings.models import BlogPost
+
+class BlogPostRudView(generics.RetrieveUpdateDestroyAPIView): # Detail view Create view Form view
+    lookup_field            = 'pk' # slug, id # url(r'?P<pk>\d+')
+    serializer_class = BlogPostSerializer
+    #queryset = BlogPost.objects.all()
+
+    def get_queryset(self):
+        return BlogPost.objects.all()
+
+    # def get_object(self):
+    #     pk = self.kwargs.get("pk")
+    #     return BlogPost.objects.get(pk=pk)
