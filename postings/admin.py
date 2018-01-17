1de3a084cd98503bdd4d7eb917f719c77f21a6f6
Daniel Leszczyński <danielleszczynski@MacBook-Pro-Daniel.local>
2018-01-17 18:40:23 +0100

    Show post values in table

--- postings/admin.py
+++ postings/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import BlogPost
 
-admin.site.register(BlogPost)
 admin.site.site_header = 'dblog admin panel'
+
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'user', 'timestamp')
+
+
+admin.site.register(BlogPost, BlogPostAdmin)
