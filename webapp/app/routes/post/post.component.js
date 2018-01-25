c740e997de4b81e163f6540284260cf29f146aa5
Daniel Leszczyński <danielleszczynski@MacBook-Pro-Daniel.local>
2018-01-26 00:42:51 +0100

    Style post

--- webapp/app/routes/post/post.component.js
+++ webapp/app/routes/post/post.component.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 export class Post extends PureComponent {
   static propTypes = {
@@ -12,6 +13,8 @@ export class Post extends PureComponent {
     this.props.fetchSinglePost(this.props.match.params.id);
   }
 
+  formatDate = (date) => moment(date).format('LLLL');
+
   render() {
     return (
       <div className="post">
@@ -19,8 +22,19 @@ export class Post extends PureComponent {
           <div className="post__hero-title">
             {this.props.post.get('title')}
           </div>
+          <div className="post__hero-date">
+            { this.formatDate(this.props.post.get('timestamp')) }
+          </div>
         </div>
 
+        <div className="post__content-wrapper">
+          <div className="post__lead">
+            {this.props.post.get('lead')}
+          </div>
+
+          <div className="post__content" dangerouslySetInnerHTML={{ __html: this.props.post.get('content') }} >
+          </div>
+        </div>
       </div>
     );
   }
