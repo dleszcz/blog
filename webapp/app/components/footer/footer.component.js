f60824393235105cfab366b112c7c318a172d2e2
Daniel Leszczyński <danielleszczynski@MacBook-Pro-Daniel.local>
2018-01-25 22:50:11 +0100

    Add Header and Footer components

---
+++ webapp/app/components/footer/footer.component.js
@@ -0,0 +1,20 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+
+export class Footer extends PureComponent {
+  static propTypes = {
+  };
+
+  componentWillMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="footer">
+        Footer
+      </div>
+    );
+  }
+}
---
+++ webapp/app/components/header/header.component.js
@@ -0,0 +1,20 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+
+export class Header extends PureComponent {
+  static propTypes = {
+  };
+
+  componentWillMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="header">
+        Header
+      </div>
+    );
+  }
+}
